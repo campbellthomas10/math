f9e7876f502ff8f5674031914edda7871ab761ac
Tommy Campbell <tjc2187@gmail.com>
2019-09-25 14:20:34 -0500

    integer division works properly

--- src/com/company/IntegerDivision.java
+++ src/com/company/IntegerDivision.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class IntegerDivision {
 
     public static void main(String[] args) {
+
+        int firstNumber;
+        int dividedBy;
+        Scanner in;
+
+        in = new Scanner(System.in);
+        System.out.println("What number are you dividing? (Must be a whole number)");
+        firstNumber = in.nextInt();
+        in = new Scanner(System.in);
+        System.out.println("Divide " + firstNumber + " by?");
+        dividedBy = in.nextInt();
+
+        System.out.println(firstNumber + " / " + dividedBy + " = " + ((firstNumber / dividedBy) + 1));
 
     }
 }
