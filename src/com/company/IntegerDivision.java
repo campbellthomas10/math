0eb0a30141f60d8e806ed098df308e122aa8cfb9
Tommy Campbell <tjc2187@gmail.com>
2019-09-25 14:31:18 -0500

    added repeat functionality

--- src/com/company/IntegerDivision.java
+++ src/com/company/IntegerDivision.java
@@ -8,20 +8,28 @@ public class IntegerDivision {
 
         int firstNumber;
         int dividedBy;
+        String continues;
         Scanner in;
 
         System.out.println("*********************************");
         System.out.println("*  INTEGER DIVISION ROUNDED UP  *");
         System.out.println("*********************************");
+        do {
+            in = new Scanner(System.in);
+            System.out.println("What number are you dividing? (Must be a whole number)");
+            firstNumber = in.nextInt();
+            in = new Scanner(System.in);
+            System.out.println("Divide " + firstNumber + " by?");
+            dividedBy = in.nextInt();
 
-        in = new Scanner(System.in);
-        System.out.println("What number are you dividing? (Must be a whole number)");
-        firstNumber = in.nextInt();
-        in = new Scanner(System.in);
-        System.out.println("Divide " + firstNumber + " by?");
-        dividedBy = in.nextInt();
+            System.out.println(firstNumber + " / " + dividedBy + " = " + ((firstNumber / dividedBy) + 1));
+            in = new Scanner(System.in);
+            System.out.println("Again? (y/n)");
+            continues = in.next();
 
-        System.out.println(firstNumber + " / " + dividedBy + " = " + ((firstNumber / dividedBy) + 1));
+        } while (!(continues.equals("n")));
+
+        System.out.println("Goodbye");
 
     }
 }
